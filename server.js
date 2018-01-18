78ef035376c029b8caa00c3aa3c15678b4345ca8
kahu2018 <fake@fake.com>
2018-01-18 15:27:29 +1300

    added heroku

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express')
+var hbs = require('express-handlebars')
+var bodyParser = require('body-parser')
+var routes = require('./routes')
+var app = express()
+module.exports = app
+
+// Middleware
+app.engine('hbs', hbs({
+  defaultLayout: 'main',
+  extname: 'hbs'
+}))
+app.set('view engine', 'hbs')
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: false }))
+
+
+
+app.use(routes)
