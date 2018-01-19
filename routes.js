fc58a8907441c13e03e6448e44882f0f2f02a95c
James Bryant <JB@James-Bryants-MacBook-Pro.local>
2018-01-19 14:16:11 +1300

    Working versiongit status! we all checked

--- routes.js
+++ routes.js
@@ -7,14 +7,6 @@ var breadArray = sandwichData.sandwich[0]
 var proteinArray = sandwichData.sandwich[1]
 var cheeseArray = sandwichData.sandwich[2]
 var vegArray = sandwichData.sandwich[3]
-<<<<<<< HEAD
-=======
-// var obj={id: [req.params.breadid, 
-//               req.params.breadid, 
-//               req.params.breadid, 
-//               req.params.breadid]
-// }
->>>>>>> 2111c5f881a36e1f5aaf1f2d463a2971ce97528c
 
 module.exports = router
 
@@ -28,9 +20,7 @@ router.get('/sandwich/home', function (req, res) {
 
 //choose bread
 router.get('/sandwich/builder', function (req, res) {
-<<<<<<< HEAD
   var baseURL = req.path
-  console.log(req.path)
   res.render('sandwich/builder', { ingredientArray: breadArray, baseURL: baseURL })
 })
 
@@ -43,32 +33,9 @@ router.get('/sandwich/builder/:bread_id/', function (req, res) {
 router.get('/sandwich/builder/:bread_id/:protein_id/', function (req, res) {
   var baseURL = req.path
   res.render('sandwich/builder', { ingredientArray: cheeseArray, baseURL: baseURL })
-=======
-  //var breadData = breadArray
-  res.render('sandwich/builder', breadData)
-})
-
-//choose cheese
-router.get('/builder/:bread_id', function (req, res) {
-  var breadId = req.params.bread_id
-  var obj = breadArray.find(function(item) {
-        return breadId == item.id 
-})
-    res.render('sandwich/builder', obj)
-})
-
-//choose protein
-router.get('/builder/:bread_id/:cheese_id', function (req, res) {
-  var cheeseId = req.params.cheese_id
-  var obj = cheeseArray.find(function(item) {
-        return cheeseId == item.id 
-})
-    res.render('sandwich/builder', obj)
->>>>>>> 2111c5f881a36e1f5aaf1f2d463a2971ce97528c
 })
 
 //choose veg
-<<<<<<< HEAD
 router.get('/sandwich/builder/:bread_id/:protein_id/:cheese_id', function (req, res) {
   var baseURL = req.path
   res.render('sandwich/builder', { ingredientArray: vegArray, baseURL: baseURL })
@@ -99,60 +66,5 @@ router.get('/sandwich/builder/:bread_id/:protein_id/:cheese_id/:veg_id', functio
   })
 
   res.render('sandwich/final', {protein: proteinObj, cheese: cheeseObj, bread: breadObj, veg: vegObj})
-=======
-router.get('/builder/:bread_id/:cheese_id/:protein_id', function (req, res) {
-  var proteinId = req.params.protein_id
-  var protein = proteinArray.find(function(item) {
-        return proteinId == item.id
-  })
-  res.render('sandwich/builder', protein)
 })
 
-//final
-router.get('/builder/:bread_id/:cheese_id/:protein_id/:veg_id', function (req, res) {
-  var breadId = req.params.bread_id
-  var cheeseId = req.params.cheese_id
-  var proteinId = req.params.protein_id
-  var vegId = req.params.veg_id
-
-  var objSandwich = {id: [breadId, cheeseId, proteinId, vegId]}
-
-
-
-  console.log(objSandwich.id)
-
-  res.render('sandwich/final', objSandwich)
->>>>>>> 2111c5f881a36e1f5aaf1f2d463a2971ce97528c
-})
-
-
-
-
-
-
-// router.get('/puppies', function (req, res) {
-//   res.render('puppies/index', puppyData)
-// })
-
-// router.get('/puppies/:id', function (req, res) {
-//   var idx = req.params.id - 1
-//   var puppyObj = puppyData.puppies[idx]
-//   puppyObj['updated'] = req.query.updated === 'true'
-//   res.render('puppies/view', puppyObj)
-// })
-
-// router.get('/puppies/edit/:id', function (req, res) {
-//   var idx = req.params.id - 1
-//   var puppyObj = puppyData.puppies[idx]
-//   res.render('puppies/edit', puppyObj)
-// })
-
-// router.post('/puppies/edit/:id', function (req, res) {
-//   var idx = req.params.id - 1
-//   puppyData.puppies[idx] = req.body
-//   fs.writeFile(__dirname + '/data.json', JSON.stringify(puppyData), (err) => {
-//     if (err) console.log(err)
-//   })
-//   res.redirect('/puppies/' + req.params.id + '/?updated=true')
-
-// })
